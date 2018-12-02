066fbd62f922e8c3a3c9d8c1df5f1d2488455d30
seyfer <seyferseed@mail.ru>
2018-12-02 17:44:06 +0100

    wrapping memo game into vue started

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <card-game/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import CardGame from './components/CardGame';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    components: {
+      CardGame,
+    },
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @import "assets/main";
 </style>
